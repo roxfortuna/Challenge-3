679b283176c56556fcf698cef19236f52c530a4c
roxfortuna <55206651+roxfortuna@users.noreply.github.com>
2019-11-17 21:33:45 -0500

    Add files via upload

---
+++ DestroyByContact.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private Done_GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <Done_GameController>();
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Boundary" || other.tag == "Enemy")
+		{
+			return;
+		}
+
+		if (explosion != null)
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
+		
+		gameController.AddScore(scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
